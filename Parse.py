a61af2831d8012a37b7ceaa3d3d77856dbe321ed
thanadolps <thanadolps@gmail.com>
2017-12-13 00:09:19 +0700

    Add bunch of stuff, mainly auto derivative graph builder

--- Parse.py
+++ Parse.py
@@ -1,8 +1,12 @@
 from graph import *
 import re
 from re import sub
+from typing import Dict
 
 # TODO [main] : Variable linking
+# TODO [main] : function support
+
+express_var : Dict[str,Variable] = {}
 
 
 def check_matching(express : str):
@@ -50,6 +54,11 @@ def pre_parse(express: str):
     # near-braclet multiplication
     express = express.replace(")(",")*(")
 
+    # multiple exponent support
+    express = express.replace("**","^")
+
+    # TODO function detecton [log(...) -> F1F_(...)]
+
     # coefficient system
     def add_start(m):
         return m.group(1) + "*" + m.group(2)
@@ -62,16 +71,30 @@ def pre_parse(express: str):
 def parse(express : str):
 
     pre = pre_parse(express)
-    return __parse(pre[::-1])
+    return __parse(pre[::-1]), express_var
+
+
+def variablize(name : str):
+    var = express_var.get(name)
+    if var is None:
+        var = Variable(name)
+        express_var[name] = var
+        return var
+    else:
+        return var
+
 
 
 def __parse(express : str):
 
     if express.isdecimal():
-        return float(express[::-1])
+        return Constance(float(express[::-1]))
 
     if express.isalpha():
-        return reduce(lambda x,y : x * y, (Variable(i) for i in express))
+
+        # TODO Detect function before reduce [linked with 57]
+
+        return reduce(lambda x,y : x * y, (variablize(i) for i in express))
 
     if express.startswith(')') and check_matching(express):
         return __parse(express[1:-1])
@@ -86,11 +109,16 @@ def __parse(express : str):
             layer -= 1
 
         if layer == 0:
-            if find_in_layer(express, '+') == -1 and find_in_layer(express, '-') == -1:
-                if j == '*':
-                    return Mul(__parse(express[i+1:]), __parse(express[:i]))
-            else:
+            if find_in_layer(express, '+-') != -1:  # + or - exist in the same layer
                 if j == '+':
                     return Add(__parse(express[i+1:]), __parse(express[:i]))
                 elif j == '-':
                     return Sub(__parse(express[i+1:]), __parse(express[:i]))
+            elif find_in_layer(express, '*/') != -1:  # * or / exist in the same layer
+                if j == '*':
+                    return Mul(__parse(express[i+1:]), __parse(express[:i]))
+                elif j == '/':
+                    return Div(__parse(express[i+1:]), __parse(express[:i]))
+            else:
+                if j == '^':
+                    return Pow(__parse(express[i+1:]), __parse(express[:i]))
--- graph.py
+++ graph.py
@@ -7,9 +7,14 @@ from functools import reduce
 # TODO [later] : Optimize Graph Calculation - coefficient combining
 # TODO [main] : auto derivative graph -> construct new derivative graph using current graph
 # TODO : Add More Function
+# TODO : Better output formatting
+# TODO : Value State Protocol [zero, one]
 
 Number = Union[int, float]
 
+
+def isNumber(x):
+    return isinstance(x, (float,int))
 
 class Graph(metaclass=ABCMeta):
 
@@ -24,6 +29,14 @@ class Graph(metaclass=ABCMeta):
     @abstractmethod
     def childs(self):
         raise NotImplementedError('users must define childs to use this base class')
+
+    @abstractmethod
+    def gradient_graph(self, var):
+        """
+
+        :rtype: Graph
+        """
+        raise NotImplementedError('users must define gradient_graph to use this base class')
 
     @staticmethod
     def constancy(x):
@@ -52,9 +65,11 @@ def draw_tree(graph : Graph, layer = 1):
 
     print(f"{tree_sep:>{len(tree_sep)*layer}}{repr(graph)}")
 
-    if graph.childs() is not None:
+    if not isinstance(graph, (int,float)) and graph.childs() is not None:
         for g in graph.childs():
             draw_tree(g, layer + 1)
+    elif isinstance(graph, (int,float)):
+        print("IF Warning")
 
 
 GraphInput = Union[Number, Graph]
@@ -70,17 +85,28 @@ class Variable(Graph):
         x = dic.get(self)
 
         if x is None:
-            raise ValueError("input dictionary doesn't contain value for {} {}".format(self.name, x))
+
+            x = dic.get(self.name)
+
+            if x is None:
+                raise ValueError("input dictionary doesn't contain value for {} {}".format(self.name, x))
 
         return x
 
     def gradient(self, dic, var):
 
         # x' = 1, k' = 0
-        if self == var:
+        if self == var or self.name == var:
             return 1
         else:
             return 0
+
+    def gradient_graph(self, var):
+
+        if self == var or self.name == var:
+            return Constance(1)
+        else:
+            return DerivitiveVariable(self, var)
 
     def childs(self):
         return None
@@ -90,6 +116,35 @@ class Variable(Graph):
 
     def __str__(self):
         return str(self.name)
+
+
+class DerivitiveVariable(Graph):
+
+    def __init__(self, variable, respect : Union[str, Variable]='x'):
+        self.var = variable
+        self.respect = respect
+
+    def eval(self, dic):
+
+        return self.var.gradient(dic, self.respect)
+
+    def gradient(self, dic, var):
+
+        return self.var.gradient(dic, var)
+
+    def gradient_graph(self, var):
+
+        return DerivitiveVariable(self, var)
+
+    def childs(self):
+        return self.var,
+
+    def __repr__(self):
+        return "Deri"
+
+    def __str__(self):
+        return str(self.var) + "'"
+
 
 
 class Constance(Graph):
@@ -104,6 +159,9 @@ class Constance(Graph):
         # k' = 0
         return 0
 
+    def gradient_graph(self, var) -> Graph:
+        return Constance(0)
+
     def childs(self):
         return None
 
@@ -113,49 +171,88 @@ class Constance(Graph):
     def __str__(self):
         return str(self.x)
 
+    def __eq__(self, other):
+        if isinstance(other, Constance):
+            return self.x == other.x
+        elif isinstance(other, (int,float)):
+            return self.x == other
+        else:
+            return False
+
+    def __float__(self):
+        return self.x
+
+zero = Constance(0)
+one = Constance(1)
+
 
 def brace(x):
-    return ("{}" if isinstance(x, Variable) or isinstance(x, Constance) else "({})").format(x)
+    if isinstance(x, Variable) or isinstance(x, Constance) or isinstance(x, DerivitiveVariable):
+        return "{}".format(x)
+    elif not None:
+        return "({})".format(x)
+    else :
+        return ''
 
 
 class Add(Graph):
 
     def __init__(self, *args):
         self.g = []
+        self.const : int = 0
 
         for i in args:
-            if isinstance(i, Add):
+            if not bool(i): # Denying
+                continue
+            elif isinstance(i, Constance): # Constance
+                self.const += i.x
+            elif isinstance(i, Add): # Add
                 self.g += i.g
-            else:
+                self.const += i.const
+            elif isinstance(i, (int, float)): # Number
+                self.const += i
+            else: # Other
                 self.g.append(self.constancy(i))
 
     def eval(self, dic) -> Number:
-        return sum(map(lambda x: x.eval(dic), self.g))
+        return self.const + sum(map(lambda x: x.eval(dic), self.g))
 
     def gradient(self, dic, var):
         return sum(map(lambda x: x.gradient(dic, var), self.g))
 
+    def gradient_graph(self, var):
+        return Add(*map(lambda x : x.gradient_graph(var), self.g))
+
     def childs(self):
-        return self.g
+        return self.g + [self.const]
 
     def __repr__(self):
         return "Add"
 
     def __str__(self):
-        return " + ".join(map(lambda x : str(x), self.g))
+        return (f"{self.const}{' + ' if self.g else ''}" if self.const else '') + \
+               " + ".join(map(lambda x : str(x), self.g))
 
 
 class Sub(Graph):
 
     def __init__(self, g1: GraphInput, g2: GraphInput):
-        self.g1 : Graph = self.constancy(g1)
-        self.g2 : Graph = self.constancy(g2)
+
+        if isinstance(g1, (Constance,int,float)) and isinstance(g2, (Constance,int,float)):
+            self.g1 = self.constancy(self.constancy(g1).x - self.constancy(g2).x)
+            self.g2 = zero
+        else:
+            self.g1: Graph = self.constancy(g1)
+            self.g2: Graph = self.constancy(g2)
 
     def eval(self, dic) -> Number:
         return self.g1.eval(dic) - self.g2.eval(dic)
 
     def gradient(self, x : Number, var):
         return self.g1.gradient(x, var) - self.g2.gradient(x, var)
+
+    def gradient_graph(self, var):
+        return Sub(self.g1.gradient_graph(var), self.g2.gradient_graph(var))
 
     def childs(self):
         return self.g1, self.g2
@@ -164,23 +261,35 @@ class Sub(Graph):
         return "Sub"
 
     def __str__(self):
-        return "{} - {}".format(self.g1, brace(self.g2))
+        return "{} {}".format('' if isinstance(self.g1, Constance) and self.g1.x == 0 else self.g1,
+                              '' if isinstance(self.g2, (Constance)) and self.g2.x == 0 else "- " + brace(self.g2))
 
 
 class Mul(Graph):
-    def __init__(self, g1: Graph , g2: Graph, *args):
+    def __init__(self, g1: Graph, *args):
         self.g : List[Graph] = []
+        self.const = 1
 
         self.add(g1)
-        self.add(g2)
         for i in args:
             self.add(i)
 
-    def add(self, x):
-            if isinstance(x, Mul):
-                self.g += x.g
-            else:
-                self.g.append(self.constancy(x))
+    def add(self, i):
+
+        if self.const == 0: # Zero = nothing
+            return
+
+        if not bool(i): # Denying
+            return
+        elif isinstance(i, Constance): # Constance
+            self.const *= i.x
+        elif isinstance(i, Mul): # Mul
+            self.g += i.g
+            self.const *= i.const
+        elif isinstance(i, (int, float)): # Number
+            self.const *= i
+        else: # Other
+            self.g.append(self.constancy(i))
 
     def eval(self, dic) -> Number:
         return reduce(lambda x, y : x * y, map(lambda x : x.eval(dic), self.g))
@@ -194,15 +303,26 @@ class Mul(Graph):
         #  f        g        h
         return sum(map(lambda x : x.gradient(dic, var) * all_mult / x.eval(dic), self.g))
 
+    def gradient_graph(self, var):
+
+        sum = Add()
+
+        for i in self.g:
+            sum += Mul(*(j if j != i else i.gradient_graph(var) for j in self.g))
+
+        return sum
+
     def childs(self):
-        return self.g
+        return self.g + [self.const]
 
     def __repr__(self):
         return "Mul"
 
     def __str__(self):
-        return " * ".join(map(lambda x: ("{}" if isinstance(x, Variable) or isinstance(x, Constance)
-                                         else "({})").format(x), self.g))
+        return (f"{self.const} * " if self.const != 1 else '') + " * ".join(map(brace, self.g)) if self.const else '0'
+
+    def __bool__(self):
+        return bool(self.g)
 
 
 class Div(Graph):
@@ -216,6 +336,9 @@ class Div(Graph):
     def gradient(self, dic, var):
         b = self.g2.eval(dic)
         return (b * self.g1.gradient(dic, var) - self.g1.eval(dic) * self.g2.gradient(dic,var)) / b**2
+
+    def gradient_graph(self, var):
+        return (self.g2 * self.g1.gradient_graph(var) - self.g1 * self.g2.gradient_graph(var)) / Pow(self.g2, 2)
 
     def childs(self):
         return self.g1, self.g2
@@ -245,6 +368,13 @@ class Pow(Graph):
         # https://www.wolframalpha.com/input/?i=Differential+(f(x))%5Eg(x)
         return (f**(g-1)) * (g * fd + (0 if not isinstance(f, Graph) else (f * log(f) * gd)))
 
+    def gradient_graph(self, var):
+        # TODO
+        f = self.g1
+        fd = self.g1.gradient_graph(var)
+        g = self.g2
+        return Pow(f, g-one) * g * fd
+
     def childs(self):
         return self.g1, self.g2
 
@@ -252,5 +382,6 @@ class Pow(Graph):
         return "Pow"
 
     def __str__(self):
-        return "{} ^ {}".format(brace(self.g1), brace(self.g2))
+        return "{} ^ {}".format(brace(self.g1), '' if isinstance(self.g2, (int,float,Constance))
+                                                                  and self.g2 == 1 else brace(self.g2))
 
